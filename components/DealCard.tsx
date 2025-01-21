97d6e3893e996cfd46117635a26d8b30486053ea
mo2men165 <moamenabdeltawab@gmail.com>
2025-01-21 14:52:08 +0200

    disbale eslint temporarily

--- components/DealCard.tsx
+++ components/DealCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 interface DealCardProps {
@@ -11,7 +12,7 @@ interface DealCardProps {
 const DealCard: React.FC<DealCardProps> = ({ image, title, description, ctaText, ctaLink }) => {
   return (
     <div className="deal-card">
-      <img src={image} alt={title} className="deal-image" />
+      <Image src={image} alt={title} className="deal-image" />
       <h3>{title}</h3>
       <p>{description}</p>
       <a href={ctaLink} className="cta-button">
